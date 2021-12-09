a765a0314975ef5edc1f7ced2dd832e8e1067b1b
2namkyung <iddbxk@daum.net>
2021-12-09 11:09:14 +0900

    webpack template react

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    output:{
+        path: path.join(__dirname, 'dist'),
+        filename: 'index_bundle.js',
+        clean: true
+    },
+
+    module:{
+        rules:[
+            {
+                test:/\.s?css$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_module/,
+                use:{
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/public/index.html'
+        }),
+
+        new CopyPlugin({
+            patterns:[
+                {from: './src/static' }
+            ]
+        })
+    ],
+
+    devServer:{
+        host:'localhost'
+    }
+}
